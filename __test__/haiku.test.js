29b54a1d9eb393ec89db82176df060f6870f4d9c
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-02 11:29:01 -0700

    it will refactor all methods into one method and pass the test for it
    Co-author: William Donovan-Seid <wdstwo@live.com>
    Co-author Ophelia Swen <ophelia.swen@gmail.com>

--- __test__/haiku.test.js
+++ __test__/haiku.test.js
@@ -8,13 +8,19 @@ describe('Haiku', () => {
     reHaiku.line1 = "splash! Silence again.";
   });
 
-  test('should turn line into a array of words', () => {
-    expect(reHaiku.getWordArray(reHaiku.line1)).toEqual(["splash!", "Silence", "again."]);
-  });
-  test('should take line and give amount of vowels in the line', () => {
-    expect(reHaiku.getNumVowels(reHaiku.line1)).toEqual(7);
-  });
-  test('should subtract any silent vowels from the number of vowels in a word', () => {
-
+//   test('should turn line into a array of words', () => {
+//     expect(reHaiku.getWordArray(reHaiku.line1)).toEqual(["splash!", "Silence", "again."]);
+//   });
+//   test('should take line and give amount of vowels in the line', () => {
+//     expect(reHaiku.getNumVowels(reHaiku.line1)).toEqual(7);
+//   });
+//   test('should subtract any silent vowels from the number of vowels in a word', () => {
+//     expect().toEqual();
+//   });
+//   test('should subtract one vowel from any diphthong the number of vowels in a word', () => {
+//     expect().toEqual();
+//   });
+  test('should return number of syllables in a line', () => {
+    expect(reHaiku.getNumSyllables(reHaiku.line1)).toEqual(5);
   });
 });
--- src/js/haiku-logic.js
+++ src/js/haiku-logic.js
@@ -5,14 +5,47 @@ export default class Haiku {
     this.line3 = "";
   }
 
-  getWordArray(line) {
-    let wordArray = line.split(" ");
-    return wordArray;
-  }
+  // // getWordArray(line) {
+  // //   let wordArray = line.split(" ");
+  // //   return wordArray;
+  // // }
 
-  getNumVowels(line) {
-    let numVowels= line.match(/[aeiouy]/ig).length
+  // // getNumVowels(line) {
+  // //   let numVowels= line.match(/[aeiouy]/ig).length;
+  // //   return numVowels;
+  // // }
   
-    return numVowels;
+  // // getNumSilentVowels(line) {
+  // //   let lineSplit = line.split(" ");
+  // //   let newArray = [];
+  // //   let numSilVow = 0;
+  // //   lineSplit.forEach(function(word){
+  // //     if(word[word.length-1]==="e"){
+  // //       numSilVow += 1;
+  // //       // let x = word.slice(0,-1);
+  // //       // newArray.push(x)
+  // //     }else{
+  // //       // let x = word;
+  // //       // newArray.push(x);
+  // //     }
+  // //   });
+  // //   return numSilVow;
+  // // }
+
+  getNumSyllables(line) {
+    let lineSplit = line.split(" ");
+    let newArray = [];
+    lineSplit.forEach(function(word){
+      if(word[word.length-1]==="e"){
+        let x = word.slice(0,-1);
+        newArray.push(x)
+      }else{
+        let x = word;
+        newArray.push(x);
+      }
+    });
+    let newLine = newArray.join(" ");
+    let numSyllables = newLine.match(/[aeiouy]{1,2}/ig).length;
+    return numSyllables;
   }
 }
